fb014679dc63da0793f53c7fee9ffc08c2d9d1a3
TimeBye <zhongziling@vip.qq.com>
2019-01-24 23:37:19 +0800

    [INI]init code

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -f "/data/mongodb.lock" ];then
+mongod --dbpath /data/db --fork --logpath /data/log/mongodb.log &
+wait $!
+touch /data/mongodb.lock
+fi
+
+redis-server &
+aria2c --conf-path=/data/aria2/aria2.conf &
+mongod -auth --bind_ip 127.0.0.1 --port 27017 --dbpath /data/db --fork --logpath /data/log/mongodb.log &
+wait $!
+
+cd /root/PyOne
+gunicorn -k eventlet -b 0.0.0.0:34567 run:app &
+wait $!
