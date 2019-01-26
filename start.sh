d357aff98f24b8bc1bf1bf26eb9b484ed1f8bca9
TimeBye <zhongziling@vip.qq.com>
2019-01-27 02:13:40 +0800

    [FIX]mongod

--- start.sh
+++ start.sh
@@ -5,19 +5,8 @@ if [ ! -f "/root/PyOne/config.py" ];then
     cp -f /root/PyOne/config.py.sample /root/PyOne/config.py
 fi
 
-if [ ! -f "/root/PyOne/mongodb.lock" ];then
-    redis-server &
-    aria2c --conf-path=/data/aria2/aria2.conf &
-    mongod --dbpath /data/db --fork --logpath /data/log/mongodb.log &
-    wait $!
-    touch /root/PyOne/mongodb.lock
-else
-    redis-server &
-    aria2c --conf-path=/data/aria2/aria2.conf &
-    mongod --auth --bind_ip 127.0.0.1 --port 27017 --dbpath /data/db --fork --logpath /data/log/mongodb.log &
-    wait $!
-fi
+redis-server &
+aria2c --conf-path=/data/aria2/aria2.conf &
+mongod --dbpath /data/db --fork --logpath /data/log/mongodb.log &
 
-cd /root/PyOne
-gunicorn -k eventlet -b 0.0.0.0:34567 run:app &
-wait $!
+cd /root/PyOne && gunicorn -k eventlet -b 0.0.0.0:34567 run:app
