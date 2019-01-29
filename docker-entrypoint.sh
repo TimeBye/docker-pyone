c47e0c8799e4736f5eae12a7550a7849c1ad279c
TimeBye <zhongziling@vip.qq.com>
2019-01-29 11:06:16 +0800

    [FIX]修复未复制版本库问题

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,48 @@
 #!/bin/bash
 
+# timezone
 ln -sf /usr/share/zoneinfo/${TZ:-"Asia/Shanghai"} /etc/localtime
 echo ${TZ:-"Asia/Shanghai"} > /etc/timezone
 
+# sshd
+if [ -n "${SSH_PASSWORD}" ];then
+    mkdir -p /var/run/sshd
+    echo root:${SSH_PASSWORD} | chpasswd
+    sed -ri 's/^#?PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+    /usr/sbin/sshd
+fi
+
+# redis
+redis-server --daemonize yes
+
+# mongodb
+mkdir -p /data/mongodb/db /data/mongodb/log
+mongod --dbpath /data/mongodb/db --fork --logpath /data/mongodb/log/mongodb.log
+
+# aria2
+mkdir -p /data/aria2/download 
+cp -rf /aria2.conf /data/aria2/aria2.conf
+sed -i "s|rpc-secret=.*|rpc-secret=${ARIA2_SECRET:-aria2-secret}|" /data/aria2/aria2.conf
+touch /data/aria2/aria2.session
+aria2c -D --conf-path=/data/aria2/aria2.conf
+
+# pyOne
+if [ ! -e "/root/PyOne/config.py" ];then
+    cp -rfa /etc/PyOne /root
+    cp -rf /root/PyOne/config.py.sample /root/PyOne/config.py
+fi
+
+# fix old version
+if [ ! -e "/root/PyOne/.git" ];then
+    cp -rfa /etc/PyOne/.git /etc/PyOne/.gitignore /root/PyOne
+fi
+
+# supervisord
+cp -rf /supervisord.conf /root/PyOne/supervisord.conf
+sed -i "s|0.0.0.0.*|0.0.0.0:${PORT:-80} run:app|" /root/PyOne/supervisord.conf
+supervisord -c /root/PyOne/supervisord.conf
+
+# crontab
 echo "* * * * * /healthcheck.sh" > /tmp/cron.`whoami`
 if [ -z ${DISABLE_REFRESH_CACHE} ];then
     REFRESH_CACHE_NEW=${REFRESH_CACHE_NEW:-"*/15 * * * *"}
@@ -18,40 +58,8 @@ else
     service cron restart
 fi
 
-if [ -n ${SSH_PASSWORD} ];then
-    mkdir -p /var/run/sshd
-    echo root:${SSH_PASSWORD} | chpasswd
-    sed -ri 's/^#?PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-    /usr/sbin/sshd
-fi
-
-if [ ! -f "/root/PyOne/config.py" ];then
-    cp -rf /etc/PyOne/* /root/PyOne
-    cp -rf /root/PyOne/config.py.sample /root/PyOne/config.py
-fi
-
-if [ ! -f "/data/aria2/aria2.conf" ];then
-    mkdir -p /data/aria2/download 
-    cp -rf /aria2.conf /data/aria2/aria2.conf
-    sed -i "s|aria2-secret|${ARIA2_SECRET:-"aria2-secret"}|" /data/aria2/aria2.conf
-    touch /data/aria2/aria2.session
-fi
-
-if [ ! -f "/data/mongodb/log/mongodb.log" ];then
-    mkdir -p /data/mongodb/db /data/mongodb/log
-fi
-
-if [ ! -f "/root/PyOne/supervisord.conf" ];then
-    cp -rf /supervisord.conf /root/PyOne/supervisord.conf
-    sed -i "s|80|${PORT:-80}|" /root/PyOne/supervisord.conf
-    touch /root/PyOne/pyone.log
-fi
-
-redis-server &
-aria2c --conf-path=/data/aria2/aria2.conf &
-mongod --dbpath /data/mongodb/db --fork --logpath /data/mongodb/log/mongodb.log &
-wait $!
-
-supervisord -c /root/PyOne/supervisord.conf
+# show log
+touch /root/PyOne/pyone.log
 tail -f /root/PyOne/pyone.log
+
 wait
